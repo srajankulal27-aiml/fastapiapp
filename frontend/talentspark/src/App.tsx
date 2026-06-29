99069b6b62b6d3b018e63cba57942045f9edbbaf
Srajan M Kulal <srajanaiml27@gmail.com>
2026-06-29 16:43:59 +0530

    complete frontend

--- frontend/talentspark/src/App.tsx
+++ frontend/talentspark/src/App.tsx
@@ -1,12 +1,17 @@
 import Welcome from "./components/Welcome";
 import NavBar from "./components/NavBar";
 import Footer from "./components/Footer";
+import CompanyCard from "./components/CompanyCard";
+import JobCard from "./components/JobCard";
+
 
 function App(){
   return(
     <>
     <NavBar/>
     <Welcome/>
+    <CompanyCard/>
+    <JobCard/>
     <Footer/>
     </>  
 

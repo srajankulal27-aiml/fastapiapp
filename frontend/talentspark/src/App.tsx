308e711e62d6210f342cee4bec2c4361f3835249
Srajan M Kulal <srajanaiml27@gmail.com>
2026-06-29 16:27:14 +0530

    complete backend

---
+++ frontend/talentspark/src/App.tsx
@@ -0,0 +1,16 @@
+import Welcome from "./components/Welcome";
+import NavBar from "./components/NavBar";
+import Footer from "./components/Footer";
+
+function App(){
+  return(
+    <>
+    <NavBar/>
+    <Welcome/>
+    <Footer/>
+    </>  
+
+  )
+}
+
+export default App
---
+++ frontend/talentspark/src/components/Welcome.tsx
@@ -0,0 +1,7 @@
+function Welcome(){
+  return(
+    <h1>Welcome to the talentspark</h1>
+  )
+}
+
+export default Welcome
